50749a65dc7c675adaf7932ea06c8faed409b1d0
AlekseyManetov <aleksey.manetov@camunda.com>
2026-02-10 14:33:30 +0100

    chore: added basic infrastructure setup(build, lint, renovate, coverage)

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+const nodeResolve = require('@rollup/plugin-node-resolve');
+
+const pkg = require('./package.json');
+const nonbundledDependencies = Object.keys({ ...pkg.dependencies });
+
+module.exports = {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    commonjs(),
+    json(),
+    nodeResolve(),
+  ],
+  external: nonbundledDependencies
+};
