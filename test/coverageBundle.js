46b9c97b73917cbe42f5841aaa08c9951a55100a
AlekseyManetov <aleksey.manetov@camunda.com>
2026-02-10 14:33:30 +0100

    chore: added basic infrastructure setup(build, lint, renovate, coverage)

--- test/coverageBundle.js
+++ test/coverageBundle.js
@@ -4,6 +4,6 @@ const allTests = require.context('.', true, /.spec\.js$/);
 
 allTests.keys().forEach(allTests);
 
-const allSources = require.context('../lib', true, /.*\.js$/);
+const allSources = require.context('../src', true, /.*\.js$/);
 
 allSources.keys().forEach(allSources);
